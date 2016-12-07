deefd74da6d0ca861981dec0cdcfd746ea1a19a0
Josh Sanders <josh5231@gmail.com>
2016-12-07 05:23:39 +0000

    Fixed server.js and added readme.md

---
+++ readme.md
@@ -0,0 +1,8 @@
+Header Parser  v1.0
+
+By Josh Sanders on 12/7/16
+
+This will return an object containing requester's IP /  Language and Operating system.
+
+The format is:
+{ "ipaddress":req.ip, "language":lang, "software":software }
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 8080;
+var bodyParser = require('body-parser');
+
+app.get('/',(req,res)=>{
+    var lang = req.headers["accept-language"].split(",")[0];
+    var software = req.headers["user-agent"].match(/\((.+?)\)/)[1];
+    var out="";
+    var out = {"ipaddress":req.ip,"language":lang,"software":software};
+   res.send(out); 
+});
+
+app.all((req,res)=>{
+   res.send(req.body); 
+});
+
+app.listen(port, function () {
+  console.log('App running on port '+port+"!");
+});
